dmitmel/crosscode-translateinator

commit 557c2981f08846a1012b0f33c5ec165d61c3aa6e

begin work on the project tree view

webpack.config.js
@@ -63,6 +63,12 @@ module.exports = (_env, { mode }) => ({
           },
           {
             loader: 'sass-loader',
+            options: {
+              sassOptions: {
+                indentedSyntax: false,
+                outputStyle: 'expanded',
+              },
+            },
           },
         ],
       },

src/backend.ts
@@ -226,7 +226,9 @@ export class Backend {
     }
   }
 
-  public async send_request(data: RequestMessageType): Promise<ResponseMessageType> {
+  public async send_request<T extends string>(
+    data: RequestMessageType,
+  ): Promise<ResponseMessageType & { type: T }> {
     if (!(this.state !== BackendState.DISCONNECTED)) {
       throw new Error('Assertion failed: this.state !== BackendState.DISCONNECTED');
     }
@@ -240,7 +242,7 @@ export class Backend {
       this.sent_request_error_callbacks.set(id, reject);
     });
     await this.send_message_internal({ type: 'req', id, data });
-    return response_promise;
+    return (response_promise as unknown) as ResponseMessageType & { type: T };
   }
 
   public disconnect(): void {

src/gui/AppMain.scss
@@ -3,8 +3,11 @@
 .App {
   width: 100%;
   height: 100%;
+  overflow: hidden;
+
   font-family: sans-serif;
-  font-size: 18px;
+  font-size: 16px;
   white-space: nowrap;
+  text-overflow: ellipsis;
   color: $color-text-normal;
 }

src/gui/AppMain.tsx
@@ -1,26 +1,59 @@
 import './AppMain.scss';
 import * as Inferno from 'inferno';
 import * as backend from '../backend';
-import * as utils from '../utils';
-import { FileTreePaneGui } from './FileTreePane';
+import { ProjectTreeGui } from './ProjectTree';
 import { BoxGui } from './Box';
 import { EditorGui } from './Editor';
 import { StatusBarGui } from './StatusBar';
+import { Event2 } from '../events';
 
-declare global {
-  // eslint-disable-next-line no-var
-  var app: AppMainGui;
-}
-
-export interface AppMainCtx {
-  backend: backend.Backend;
+export interface AppMainGuiCtx {
+  app: AppMain;
 }
 
 export class AppMainGui extends Inferno.Component<unknown, unknown> {
-  public backend: backend.Backend;
+  public inner = new AppMain();
 
-  public constructor(props: unknown, context: unknown) {
-    super(props, context);
+  public getChildContext(): AppMainGuiCtx {
+    return { app: this.inner };
+  }
+
+  public componentDidMount(): void {
+    void this.inner.connect();
+  }
+
+  public componentWillUnmount(): void {
+    this.inner.disconnect();
+  }
+
+  public render(): JSX.Element {
+    return (
+      <BoxGui orientation="vertical" className="App">
+        <BoxGui orientation="horizontal" className="BoxItem-expand">
+          <ProjectTreeGui />
+          <EditorGui className="BoxItem-expand" />
+        </BoxGui>
+        <StatusBarGui />
+      </BoxGui>
+    );
+  }
+}
+
+declare global {
+  // eslint-disable-next-line no-var
+  var app: AppMain;
+}
+
+export class AppMain {
+  public backend: backend.Backend;
+  public current_project_id: number | null = null;
+  public current_project_meta: { translation_locale: string } | null = null;
+  public events = {
+    project_opened: new Event2(),
+    project_closed: new Event2(),
+  };
+
+  public constructor() {
     if ('app' in window) {
       throw new Error("Assertion failed: !('app' in window)");
     }
@@ -32,37 +65,31 @@ export class AppMainGui extends Inferno.Component<unknown, unknown> {
     this.backend = new backend.Backend();
   }
 
-  public getChildContext(): AppMainCtx {
-    return { backend: this.backend };
+  public async connect(): Promise<void> {
+    await this.backend.connect();
+
+    {
+      let response = await this.backend.send_request<'Project/open'>({
+        type: 'Project/open',
+        dir: '/home/dmitmel/Projects/Rust/crosscode-localization-engine/tmp',
+      });
+      this.current_project_id = response.project_id;
+    }
+
+    {
+      let response = await this.backend.send_request<'Project/get_meta'>({
+        type: 'Project/get_meta',
+        project_id: this.current_project_id,
+      });
+      this.current_project_meta = {
+        translation_locale: response.translation_locale,
+      };
+    }
+
+    this.events.project_opened.fire();
   }
 
-  public componentDidMount(): void {
-    (async () => {
-      await this.backend.connect();
-      while (true) {
-        let { project_id } = (await this.backend.send_request({
-          type: 'Project/open',
-          dir: '/home/dmitmel/Projects/Rust/crosscode-localization-engine/tmp',
-        })) as backend.ResponseMessageType & { type: 'Project/open' };
-        await this.backend.send_request({ type: 'Project/close', project_id });
-        await utils.wait(3000);
-      }
-    })();
-  }
-
-  public componentWillUnmount(): void {
+  public disconnect(): void {
     this.backend.disconnect();
-  }
-
-  public render(): JSX.Element {
-    return (
-      <BoxGui orientation="vertical" className="App">
-        <BoxGui orientation="horizontal" className="BoxItem-expand">
-          <FileTreePaneGui />
-          <EditorGui className="BoxItem-expand" />
-        </BoxGui>
-        <StatusBarGui />
-      </BoxGui>
-    );
   }
 }

src/gui/Box.tsx
@@ -1,16 +1,27 @@
-import * as Inferno from 'inferno';
 import './Box.scss';
 
-export interface BoxProps extends Inferno.Props<typeof BoxGui> {
+export interface BoxGuiProps extends HTMLAttributes<HTMLDivElement> {
   orientation: 'vertical' | 'horizontal';
   reverse_children?: boolean;
   scroll?: boolean;
 }
 
-export function BoxGui(props: BoxProps): JSX.Element {
-  let clazz = `Box Box-orientation-${props.orientation}`;
-  if (props.reverse_children) clazz += ' Box-reverse_children';
-  if (props.scroll) clazz += ' Box-scroll';
-  if (props.className != null) clazz += ` ${props.className}`;
-  return <div className={clazz}>{props.children}</div>;
+export function BoxGui({
+  orientation,
+  reverse_children,
+  scroll,
+  className,
+  class: _class,
+  children,
+  ...rest
+}: BoxGuiProps): JSX.Element {
+  let clazz = `Box Box-orientation-${orientation}`;
+  if (reverse_children) clazz += ' Box-reverse_children';
+  if (scroll) clazz += ' Box-scroll';
+  if (className != null) clazz += ` ${className}`;
+  return (
+    <div className={clazz} {...rest}>
+      {children}
+    </div>
+  );
 }

src/gui/Editor.tsx
@@ -3,7 +3,9 @@ import * as Inferno from 'inferno';
 import { BoxGui } from './Box';
 import { IconGui } from './Icon';
 
-export function EditorGui(props: Inferno.Props<typeof EditorGui>): JSX.Element {
+export interface EditorGuiProps extends Inferno.Props<typeof EditorGui> {}
+
+export function EditorGui(props: EditorGuiProps): JSX.Element {
   let clazz = 'Editor';
   if (props.className != null) clazz += ` ${props.className}`;
   return (
@@ -14,9 +16,9 @@ export function EditorGui(props: Inferno.Props<typeof EditorGui>): JSX.Element {
   );
 }
 
-export interface EditorTabListProps extends Inferno.Props<typeof EditorTabListGui> {}
+export interface EditorTabListGuiProps extends Inferno.Props<typeof EditorTabListGui> {}
 
-export function EditorTabListGui(props: EditorTabListProps): JSX.Element {
+export function EditorTabListGui(props: EditorTabListGuiProps): JSX.Element {
   let clazz = 'EditorTabList';
   if (props.className != null) clazz += props.className;
   return (
@@ -39,13 +41,13 @@ const EDITOR_TAB_ICONS = new Map<EditorTabType, string>([
   ['game_file', 'file-earmark-text-fill'],
 ]);
 
-export interface EditorTabProps extends Inferno.Props<typeof EditorTabGui> {
+export interface EditorTabGuiProps extends Inferno.Props<typeof EditorTabGui> {
   type: EditorTabType;
   name: string;
   active: boolean;
 }
 
-export function EditorTabGui(props: EditorTabProps): JSX.Element {
+export function EditorTabGui(props: EditorTabGuiProps): JSX.Element {
   let clazz = 'EditorTab';
   if (props.active) clazz += ' EditorTab-active';
   let icon = EDITOR_TAB_ICONS.get(props.type)!;

src/gui/FileTreePane.scss
@@ -1,6 +1,0 @@
-@import '../theme.scss';
-
-.FileTreePane {
-  background-color: $color-FileTreePane;
-  min-width: 300px;
-}

src/gui/FileTreePane.tsx
@@ -1,6 +1,0 @@
-import * as Inferno from 'inferno';
-import './FileTreePane.scss';
-
-export function FileTreePaneGui(): JSX.Element {
-  return <div className="FileTreePane">Hi!</div>;
-}

commit 55b73d7dd39949438a0017947b387c1afa4af517

compress the CSS in production
This bug got introduced in 557c2981f08846a1012b0f33c5ec165d61c3aa6e

webpack.config.js
@@ -66,7 +66,7 @@ module.exports = (_env, { mode }) => ({
             options: {
               sassOptions: {
                 indentedSyntax: false,
-                outputStyle: 'expanded',
+                outputStyle: mode === 'production' ? 'compressed' : 'expanded',
               },
             },
           },