7dda25efb470086f116a8d613db2755bd9e78f0f
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2021-02-20 13:32:45 +0200

    add a basic file tree (which displays the entire hierarchy at once)

--- src/gui/ProjectTree.tsx
+++ src/gui/ProjectTree.tsx
@@ -7,12 +7,16 @@ import cc from 'classcat';
 
 export interface ProjectTreeGuiState {
   translation_locale: string | null;
+  translation_files: PathTree | null;
+  virtual_game_files: PathTree | null;
 }
 
 export class ProjectTreeGui extends Inferno.Component<unknown, unknown> {
   public context!: AppMainGuiCtx;
   public state: ProjectTreeGuiState = {
     translation_locale: null,
+    translation_files: null,
+    virtual_game_files: null,
   };
 
   public componentDidMount(): void {
@@ -27,13 +31,33 @@ export class ProjectTreeGui extends Inferno.Component<unknown, unknown> {
     app.events.project_closed.off(this.on_project_closed);
   }
 
-  private on_project_opened = (): void => {
+  private on_project_opened = async (): Promise<void> => {
     let { app } = this.context;
-    this.setState({ translation_locale: app.current_project_meta?.translation_locale });
+    this.setState({ translation_locale: app.current_project_meta!.translation_locale });
+
+    {
+      let response = await app.backend.send_request<'Project/list_tr_files'>({
+        type: 'Project/list_tr_files',
+        project_id: app.current_project_id!,
+      });
+      this.setState({ translation_files: paths_list_to_tree(response.paths) });
+    }
+
+    {
+      let response = await app.backend.send_request<'Project/list_virtual_game_files'>({
+        type: 'Project/list_virtual_game_files',
+        project_id: app.current_project_id!,
+      });
+      this.setState({ virtual_game_files: paths_list_to_tree(response.paths) });
+    }
   };
 
   private on_project_closed = (): void => {
-    this.setState({ translation_locale: null });
+    this.setState({
+      translation_locale: null,
+      translation_files: null,
+      virtual_game_files: null,
+    });
   };
 
   public render(): JSX.Element {
@@ -43,12 +67,16 @@ export class ProjectTreeGui extends Inferno.Component<unknown, unknown> {
         <div className="ProjectTree-Header">
           <IconGui icon={null} /> PROJECT [{translation_locale}]
         </div>
-        <ProjectTreeSectionGui name="Translation files">
-          <FileTreeGui />
-        </ProjectTreeSectionGui>
-        <ProjectTreeSectionGui name="Game files">
-          <FileTreeGui />
-        </ProjectTreeSectionGui>
+        {this.state.translation_files != null ? (
+          <ProjectTreeSectionGui name="Translation files">
+            <FileTreeGui data={this.state.translation_files} />
+          </ProjectTreeSectionGui>
+        ) : null}
+        {this.state.virtual_game_files != null ? (
+          <ProjectTreeSectionGui name="Game files">
+            <FileTreeGui data={this.state.virtual_game_files} />
+          </ProjectTreeSectionGui>
+        ) : null}
       </BoxGui>
     );
   }
@@ -87,14 +115,69 @@ export class ProjectTreeSectionGui extends Inferno.Component<ProjectTreeSectionG
           onClick={this.on_name_click}>
           <IconGui icon={is_opened ? 'chevron-down' : 'chevron-right'} /> {this.props.name}
         </div>
-        {is_opened ? this.props.children : null}
+        {is_opened ? (
+          // TODO: use a simple div with overflow: auto
+          <BoxGui orientation="vertical" scroll>
+            {this.props.children}
+          </BoxGui>
+        ) : null}
       </BoxGui>
     );
   }
 }
 
-export class FileTreeGui extends Inferno.Component<unknown, unknown> {
+export interface FileTreeGuiProps {
+  data: PathTree;
+}
+
+export class FileTreeGui extends Inferno.Component<FileTreeGuiProps, unknown> {
   public render(): JSX.Element {
-    return <div className="ProjectTreeItem">Hi!</div>;
+    let elements: JSX.Element[] = [];
+    this.render_tree(this.props.data, [], elements);
+    return <Inferno.Fragment $HasKeyedChildren>{elements}</Inferno.Fragment>;
   }
+
+  private render_tree(tree: PathTree, stack: string[], dst: JSX.Element[]): void {
+    for (let [subtree_name, subtree] of tree) {
+      let depth = stack.length;
+      stack.push(subtree_name);
+      let full_path = stack.join('/');
+      let is_directory = subtree.size > 0;
+
+      dst.push(
+        <div
+          key={full_path}
+          class="ProjectTreeItem"
+          style={{ 'padding-left': `${depth * 8 + 6}px` }}
+          title={full_path}
+          tabIndex={0}>
+          <IconGui icon={is_directory ? 'folder2-open' : 'file-earmark-text'} /> {subtree_name}
+        </div>,
+      );
+
+      if (is_directory) {
+        this.render_tree(subtree, stack, dst);
+      }
+      stack.pop();
+    }
+  }
+}
+
+export type PathTree = Map<string, PathTree>;
+export function paths_list_to_tree(paths: string[]): PathTree {
+  let root_dir: PathTree = new Map();
+
+  for (let path of paths) {
+    let current_dir: PathTree = root_dir;
+    for (let component of path.split('/')) {
+      let next_dir = current_dir.get(component);
+      if (next_dir == null) {
+        next_dir = new Map();
+        current_dir.set(component, next_dir);
+      }
+      current_dir = next_dir;
+    }
+  }
+
+  return root_dir;
 }
